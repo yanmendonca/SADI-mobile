619640758f715526aed56a6a9085be7ced5751f8
Henrique Tosta <henrique.tosta@institutoanexo.com.br>
2024-09-09 16:01:32 -0300

    feat: Added a way to display local videos aswell

--- app/telatres.tsx
+++ app/telatres.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import { SafeAreaView, ScrollView, View, Text, Pressable, StyleSheet, TouchableOpacity, Linking } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { SafeAreaView, ScrollView, View, Text, Pressable, StyleSheet, TouchableOpacity, Linking, Button, Modal } from 'react-native';
 import { ClassCard, TutorialsCard, SimulatedExamCard } from '@/components/Cards';
 import { SubjectHeader } from '@/components/Headers';
+import { Video, ResizeMode } from 'expo-av';
 import { router } from 'expo-router';
 import Constants from 'expo-constants';
 
 export default function App(): React.JSX.Element {
+  const videoRef = useRef(null);
+  const [modalVisible, setModalVisible] = useState(false);
+
   const openYouTube = (url: string) => {
     Linking.openURL(url).catch((err) => console.error('Erro ao abrir URL', err));
+  };
+
+  const openLocalVideo = () => {
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
   };
 
   return (
@@ -25,7 +37,8 @@ export default function App(): React.JSX.Element {
 
         <View style={{ paddingLeft: 16 }}>
           <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-            <TouchableOpacity onPress={() => openYouTube('https://www.youtube.com/watch?v=Dm03CZ5mpNs')}>
+            
+            <TouchableOpacity onPress={openLocalVideo}>
               <TutorialsCard
                 title="Eletroquímica"
                 duration="60 min"
@@ -64,11 +77,29 @@ export default function App(): React.JSX.Element {
                 imageSource={require('@/assets/images/TutorialsThumb/QuimAmb.png')}
               />
             </TouchableOpacity>
-
           </ScrollView>
         </View>
 
         <SimulatedExamCard />
+
+        <Modal
+          visible={modalVisible}
+          animationType="slide"
+          transparent={true}
+          onRequestClose={closeModal}
+        >
+          <View style={styles.modalContainer}>
+            <Video
+              ref={videoRef}
+              style={styles.video}
+              source={require('@/assets/Videos/VideoTest.mp4')}
+              useNativeControls
+              resizeMode={ResizeMode.CONTAIN}
+              isLooping
+            />
+            <Button title="Fechar" onPress={closeModal} />
+          </View>
+        </Modal>
 
       </ScrollView>
     </SafeAreaView>
@@ -95,5 +126,15 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: '600',
     color: '#1d4ed8',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+  },
+  video: {
+    width: '90%',
+    height: 200,
   },
 });
