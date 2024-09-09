d5c15c0a5ab6ac807d5e69d049aadf4e778f92f9
Yan <yan.mendonca@anexoenergia.com.br>
2024-09-09 16:00:18 -0300

    Merge Adjustments

--- app/learn/index.tsx
+++ app/learn/index.tsx
@@ -1,11 +1,22 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { Redirect } from 'expo-router';
 import Question from '@/screens/question'
+import { SubjectList as SubjectListScreen } from "@/screens/prepare/SubjectList";
 
 export default function Modal() {
   return (
     // <Redirect href='/learn/Química_Orgânica/1'/>
-    <Question/>
+    // <Question/>
+    <SubjectListScreen 
+                areaName="CIÊNCIAS DA NATUREZA E SUAS TECNOLOGIAS" 
+                subjects={
+                    [
+                        {name: "Física", done: true, totalMinutes: 120, image: require("@/assets/images/focus.jpg")},
+                        {name: "Química", done: false, totalMinutes: 120, image: require("@/assets/images/focus.jpg")},
+                        {name: "Biologia", done: false, totalMinutes: 120, image: require("@/assets/images/focus.jpg")}
+                    ]
+                }>
+            </SubjectListScreen>
   );
 }
 
